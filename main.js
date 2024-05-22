028990e59551ae3e2f405e8f2544b60ada144a61
A-Sato0422 <achira0422@gmail.com>
2024-05-22 15:45:00 +0900

    一覧ページを追加しました。

--- main.js
+++ main.js
@@ -1,32 +1,35 @@
 'use strict';
 {
-    // menu-bar
-    const bookCloseNav = document.querySelector('.book-close-btn');
-    const bookOpenNav = document.querySelector('.book-open-btn');
-    const menuBar = document.querySelector('.menu-bar');
+    //loading機能 
 
-    bookCloseNav.addEventListener('click', () => {
-        menuBar.classList.toggle('active');
-        bookCloseNav.classList.toggle('active');
-        bookOpenNav.classList.toggle('active');
+    //テキストのカウントアップ+バーの設定
+    const bar = new ProgressBar.Line(splash_text, {//id名を指定
+        easing: 'easeInOut',//アニメーション効果linear、easeIn、easeOut、easeInOutが指定可能
+        duration: 1000,//時間指定(1000＝1秒)
+        strokeWidth: 0.5,//進捗ゲージの太さ
+        color: '#222',//進捗ゲージのカラー
+        trailWidth: 0.5,//ゲージベースの線の太さ
+        trailColor: '#fff',//ゲージベースの線のカラー
+        text: {//テキストの形状を直接指定       
+            style: {//天地中央に配置
+                position: 'absolute',
+                left: '50%',
+                top: '50%',
+                padding: '0',
+                margin: '-30px 0 0 0',//バーより上に配置
+                transform: 'translate(-50%,-50%)',
+                'font-size': '2rem',
+                color: '#000',
+            },
+            autoStyleContainer: false //自動付与のスタイルを切る
+        },
+        step: function (state, bar) {
+            bar.setText(Math.round(bar.value() * 100) + ' %'); //テキストの数値
+        }
     });
 
-    bookOpenNav.addEventListener('click', () => {
-        menuBar.classList.toggle('active');
-        bookCloseNav.classList.toggle('active');
-        bookOpenNav.classList.toggle('active');
-    });
-
-    // carousel effect
-    $(function () {
-        $(".slide-items").slick({
-            autoplay: true,
-            autoplaySpeed: 2000,
-            infinite: true,
-            fade: true,
-            cssEase: 'linear',
-            speed: 600,
-            arrows: true
-        });
+    //アニメーションスタート
+    bar.animate(1.0, function () {//バーを描画する割合を指定します 1.0 なら100%まで描画します
+        $("#splash").delay(200).fadeOut(400);//アニメーションが終わったら#splashエリアをフェードアウト
     });
 }
