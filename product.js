4d3a819d6722280a68f22a96a32a3f135847809b
seiya <iipshimaguchi117@gmail.com>
2024-05-27 14:40:46 +0900

    JavaScript挿入

--- product.js
+++ product.js
@@ -1,11 +1,8 @@
-$(document).ready(function(){
-  $('.image-slider').slick({
-      dots: true, // ドットナビゲーションを表示
-      infinite: true, // 無限ループ
-      speed: 500, // トランジションの速度
-      slidesToShow: 1, // 表示するスライド数
-      slidesToScroll: 1, // スクロールするスライド数
-      autoplay: true, // 自動再生
-      autoplaySpeed: 2000 // 自動再生のスピード
-  });
+'use strict';
+
+const thumbs = document.querySelectorAll('.thumb');
+thumbs.forEach(function(item, index) {
+    item.onclick = function() {
+        document.getElementById('bigimg').src = this.dataset.image;
+    }
 });
