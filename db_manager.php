64c3953814a2ac5cada2a0f1284defe5c92b7fc8
seiya <iipshimaguchi117@gmail.com>
2024-05-31 14:39:37 +0900

    サーバー変更

--- db_manager.php
+++ db_manager.php
@@ -1,6 +1,6 @@
 <?php
 function getDb() : PDO {
-  $dsn = 'mysql:dbname=LAA1586069-teamb; host=	mysql305.phy.lolipop.lan; charset=utf8';
+  $dsn = 'mysql:dbname=LAA1586069-teamb; host=mysql305.phy.lolipop.lan; charset=utf8';
   $usr = 'LAA1586069';
   $passwd = 'teamdb';
 
