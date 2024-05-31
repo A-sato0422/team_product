21744db889d228dd206eb927f4267d3bcc7f9979
seiya <iipshimaguchi117@gmail.com>
2024-05-31 14:35:53 +0900

    データベースアクセス情報変更

--- db_manager.php
+++ db_manager.php
@@ -1,7 +1,7 @@
 <?php
 function getDb() : PDO {
   $dsn = 'mysql:dbname=LAA1586069-teamb; host=	mysql305.phy.lolipop.lan; charset=utf8';
-  $usr = '	LAA1586069';
+  $usr = 'LAA1586069';
   $passwd = 'teamdb';
 
   $db = new PDO($dsn, $usr, $passwd);
